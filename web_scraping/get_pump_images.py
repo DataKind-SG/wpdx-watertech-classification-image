9beeee91094d49b3fceaf61d0f3625c90a28dbe7
kelvin <btkelvin@gmail.com>
2019-08-30 21:28:06 +0800

    editing get_pump_images.py and downloaded 100 images per class

--- web_scraping/get_pump_images.py
+++ web_scraping/get_pump_images.py
@@ -1,30 +1,43 @@
 import pandas as pd
-from bs4 import BeautifulSoup
 import requests   # downloading images
 import shutil     # downloading images
 import os   
 import uuid # generator for file names
 
 
-data = pd.read_csv("water_tech_image_links.csv")
+data = pd.read_csv("water_tech_image_links_cleaned.csv")
 links = data[data.photo_lnk.notnull()]
+links.loc[:, 'clean_water_tech'] = links.clean_water_tech.str.strip()
 
 ### Remove any pre-existing files
-os.remove("../data/water_tech_dictionary.csv")
 shutil.rmtree('../data/images')
 os.mkdir("../data/images")
 
-###  Downloading images
-mapping = pd.DataFrame(columns = ["water_tech","fileName"])
-maxImages = 1000
-for i in range(min(links,maxImages)):
-    r = requests.get(links.photo_lnk.iloc[i], stream = True)
-    if r.status_code == 200:
-        fileName = str(uuid.uuid4()) + ".jpg"
-        with open(os.path.join("../data/images/",fileName), 'wb') as f:
-            r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, f)
-            
-            mapping = mapping.append({"water_tech": links.water_tech.iloc[i], "fileName": fileName}, ignore_index = True)
+num_pics_per_class = 100
 
-mapping.to_csv("../data/water_tech_dictionary.csv")
+###  Downloading images
+for x in links.clean_water_tech.unique():
+    
+    links_class = links[links.clean_water_tech == x]
+    tries = 0
+    success = 0
+    
+    while ((success < num_pics_per_class) & (len(links_class.index) > tries)):
+
+        r = requests.get(links_class.photo_lnk.iloc[tries], stream = True)
+        tries += 1
+        
+        if r.status_code == 200:
+
+            success += 1
+            fileName = str(uuid.uuid4()) + ".jpg"
+            
+            # make sub-directory for class if not present
+            os.makedirs(os.path.join("../data/images", x), exist_ok=True)
+            
+            # save image to sub-directory
+            with open(os.path.join("../data/images", x, fileName),
+                      'wb') as f:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, f)
+                
